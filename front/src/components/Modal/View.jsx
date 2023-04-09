a3db82ac41408a64996fc8c372f2f4dffe41a5df
Samuel Lee <hla294@sfu.ca>
2023-04-08 17:58:58 -0700

    edit modal

--- front/src/components/Modal/View.jsx
+++ front/src/components/Modal/View.jsx
@@ -1,28 +1,65 @@
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Button, Divider, TextField, Typography } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react';
 
 const View = () => {
 
   return (
     <>              
-        <Box sx={{ my: 1.5, px: 2.5 }}>
-        <Typography variant="h4" noWrap>
-        title
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-        tag
-        </Typography>
-        <Typography variant="subtile2" sx={{ color: 'text.secondary' }} noWrap>
-        date
-        </Typography>
-        </Box>
-        <Divider/>
-        <Box sx={{ my: 4, px: 4 }}>
-        <Typography variant='container'>
+        <Box sx={{ position:"center"}}>
+            <Typography variant="h4" noWrap>
+                Title
+            </Typography>
 
-        </Typography>
+            <Typography variant="subtile2" sx={{ 
+                color: 'text.secondary', 
+                backgroundColor: '#FCD12A',
+                borderRadius: 1,
+                alignItems: 'center',
+                paddingRight: '5px',
+                paddingLeft: '5px',
+                marginRight: '7px'
+                 }} noWrap>
+                tag
+            </Typography>
+
+            <Typography variant="subtile2" sx={{ 
+                color: 'text.secondary', 
+                backgroundColor: '#FCD12A',
+                borderRadius: 1,
+                alignItems: 'center',
+                paddingRight: '5px',
+                paddingLeft: '5px',
+                marginRight: '7px',
+                 }} noWrap>
+                deadline
+            </Typography>
+            {"\n"}
+            <Typography variant="subtile2" sx={{ color: 'text.secondary' }} noWrap>
+                date
+            </Typography>
         </Box>
-        <Divider sx={{ borderStyle: 'dashed' }} />      
+        <Divider sx={{paddingBottom: '16px'}}/>
+
+        <Box sx={{ my: 4, px: 0 }}>
+            <Typography variant='container'>
+                
+            </Typography>
+        </Box>
+
+        <Divider sx={{ borderStyle: 'solid', marginBottom: '35px' }} /> 
+
+        <Box>
+            <Typography >
+            Comments
+            </Typography>
+            <TextField label="Leave a comment" fullWidth
+                        sx = {{ height: 'medium',
+                                marginTop: '5px'}}
+                         />
+            <Button variant='contained' color="primary" sx={{ mt:1.5, bgcolor: 'grey', marginRight:'auto'}}>
+                confirm
+            </Button>
+        </Box>   
     </>
   );
 };
